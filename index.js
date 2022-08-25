c0f22e8a730951d70296a455e457a9a7d4de2443
ogola5 <ogolaevance5@gmail.com>
2022-08-26 02:20:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+async function submitData(name,email){
+    const formData = {
+        name:"Evance",
+        email:"ogolaevans@gmail.com",
+    }
+
+    const configurationObject = {
+        method:"POST",
+        headers:{
+            "Content_Type":"application/json",
+            Accept:"application/json",
+        },
+        body:JSON.stringify(formData),
+    };
+    try {
+        const response = await fetch("http://localhost:3000/users", configurationObject);
+        const object = await response.json();
+        const p = document.createElement('p');
+        return document.querySelector('body').appendChild(p).textContent = object.id;
+        
+        
+    } catch (error) {
+        const p_1 = document.createElement('p');
+        return document.querySelector('body').appendChild(p_1).textContent = error;
+    
+    }
+}
